99697a764850a5b5a39b99a014d34a5c2c0b15c3
Blurry Face <karanrampal@users.noreply.github.com>
2024-09-03 10:32:21 +0200

    Update query

--- src/bq_manager/query.py
+++ src/bq_manager/query.py
@@ -2,46 +2,25 @@
 
 import pandas as pd
 from google.cloud import bigquery
-from ydata_profiling import ProfileReport
 
 from config_manager.manager import Params
 
 
-class BQManager:
+class BQManager:  # pylint: disable=too-few-public-methods
     """Manage BigQuery"""
 
     def __init__(self, configs: Params) -> None:
         self.bq_client = bigquery.Client(project=configs.project_id)
         self.filter_cols = configs.filter_columns
 
-    def execute_sql(self, query: str) -> pd.DataFrame:
+    def execute_sql(self, query_: str) -> pd.DataFrame:
         """Execute SQL query in Bigquery"""
-        if not query:
+        if not query_:
             return pd.DataFrame()
         try:
-            ans = self.bq_client.query(query.strip()).result().to_dataframe()
+            ans = self.bq_client.query(query_.strip()).result().to_dataframe()
+            if ans.size == 1:
+                ans = ans.astype(str)
         except Exception as exc:  # pylint: disable=broad-except
-            ans = pd.DataFrame(data={"Error": exc}, index=[0])
+            ans = pd.DataFrame({"Error": [exc]})
         return ans
-
-    def data_profiling(self, table_name: str) -> str:
-        """Run data profiling tool"""
-        table_split = table_name.split(".")
-        col_query = f"""
-        SELECT column_name
-        FROM `{".".join(table_split[:-1])}`.INFORMATION_SCHEMA.COLUMNS
-        WHERE table_name = "{table_split[-1]}"
-        """
-        col_names = self.bq_client.query(col_query).result().to_dataframe()
-        tmp = col_names[~col_names["column_name"].isin(self.filter_cols)].column_name.tolist()
-        tmp = [x for x in tmp if "date" not in x]
-        col_names = ", ".join(tmp)
-
-        query = f"""
-        SELECT  {col_names}
-        FROM  `{table_name}`
-        LIMIT  50000
-        """
-        res = self.bq_client.query(query).result().to_dataframe()
-        profile = ProfileReport(res)
-        return profile.to_html()
